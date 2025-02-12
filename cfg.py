a518820151a00698c39ebd661253e449e081611a
Sadman Hossain Sajid <82148534+sadman128@users.noreply.github.com>
2025-02-12 15:22:15 +0600

    Create cfg.py

---
+++ cfg.py
@@ -0,0 +1,11 @@
+class config:
+
+    BOT_TOKEN = "token"  # bot token
+
+    GUILD = 1005146395004641460 # server id
+    ROLE_USER = 1260907837358477344 # a role to use zring etc
+    ROLE_ADMIN = 1005153072290480168 #admin role
+    CHANNEL_INFO = 1317427043167895572 # stat channel where stats will be diplayed
+    DELETE_LOG = 1317940091452391445 # delete msg info
+    RESET_INFO = 1010615172076941354 # stat reset msg channel
+    AUTHOR = [634826720293290004,324549948165324802] # kenji and bignoob [ authors who have permission to reset the stats]
