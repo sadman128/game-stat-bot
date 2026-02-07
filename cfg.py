61752b9563a0eafe066335116dfce0266cc3475b
Sadman Hossain Sajid <82148534+sadman128@users.noreply.github.com>
2026-02-07 16:29:33 +0600

    Refactor config class with additional parameters

--- cfg.py
+++ cfg.py
@@ -1,11 +1,34 @@
 class config:
+    BOT_TOKEN = 
 
-    BOT_TOKEN = "token"  # bot token
+    GUILD = 1005146395004641460 #bogs server
+    ROLE_USER = 1260907837358477344 # botcum
+    ROLE_ADMIN = 1260907837358477344 # botcum
 
-    GUILD = 1005146395004641460 # server id
-    ROLE_USER = 1260907837358477344 # a role to use zring etc
-    ROLE_ADMIN = 1005153072290480168 #admin role
-    CHANNEL_INFO = 1317427043167895572 # stat channel where stats will be diplayed
+    CHANNEL_INFO = 1317427043167895572 # stat channel
+    CHANNEL_LIFELESS = 1445885462681092337 # life less counter
+
     DELETE_LOG = 1317940091452391445 # delete msg info
-    RESET_INFO = 1010615172076941354 # stat reset msg channel
-    AUTHOR = [634826720293290004,324549948165324802] # kenji and bignoob [ authors who have permission to reset the stats]
+    JOIN_LOG = 1461670667593318400 # join and leave vc info
+
+    RESET_INFO = 1010615172076941354 # stat reset msg
+    YAP_CHAT = 1005146395793174600
+
+    AUTHOR = [634826720293290004,324549948165324802] # kenji and bignoob
+
+    AI_TOKEN = 
+    AI_BASE_URL = 
+    AI_CHANNEL = 1453463533420281996
+
+    AI_SYSTEM_PROMPT = '''You are the AI assistant for the Discord gaming server “Basement of Gamers (BOGS)”, managed by kenji.xx.
+    Members discuss gaming and other various topics for research,education and fun.Be factual,honest,direct,and candid(not unnecessarily censored).
+    Keep replies under 500 characters,short,simple,straight to the point, with light humor.'''
+
+    DB_CONFIG = {
+    "host": ,
+    "user": ,
+    "password": ,
+    "port": ,
+    "database": ,
+}
+
